29705681797fc6885dd7196eb268f76560b19eb4
Toffifi <sanyq@mail.ru>
2020-04-03 16:53:03 +0300

    feat: add keyboard

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
 import './style.scss'
+import Keyboard from './keyboard'
+
+const keyboardObj = new Keyboard();
+
+const main = document.createElement('main');
+const keyboard = document.createElement('section');
+const field = document.createElement('section');
+
+
+keyboard.className = 'keyboard';
+field.className = 'field';
+
+document.querySelector('body').appendChild(main);
+document.querySelector('main').appendChild(field);
+document.querySelector('main').appendChild(keyboard);
+
+field.innerHTML = '<form> <textarea autofocus class="input" rows="12" cols="89"></textarea> </form>'
+
+const input = document.querySelector('textarea');
+
+
+keyboardObj.initialize(input, keyboard);
+
+
+
+document.addEventListener('keydown', (event) => {
+    const { func, char } = keyboardObj.getKey(event.keyCode);
+    if (char) {
+        event.preventDefault();
+        input.value += char;
+    } else if (func) {
+        event.preventDefault();
+        console.log(func)
+        //func();
+        //do the function
+    }
+})
+
+document.addEventListener('keyup', (event) => {
+    event.preventDefault();
+    keyboardObj.getKey(event.keyCode);
+})
---
+++ src/keys.js
@@ -0,0 +1,914 @@
+const keys = [
+    {
+        id: 192,
+        size: 1,
+        func: false,
+        name: {
+            en: {
+                normal: '`',
+                shifted: '~',
+                double: true
+            },
+            ru: {
+                normal: 'ё',
+                shifted: 'Ё',
+                double: false
+            }
+        }
+    },
+    {
+        id: 49,
+        size: 1,
+        func: false,
+        name: {
+            en: {
+                normal: '1',
+                shifted: '!',
+                double: true
+            },
+            ru: {
+                normal: '1',
+                shifted: '!',
+                double: true
+            }
+        }
+    },
+    {
+        id: 50,
+        size: 1,
+        func: false,
+        name: {
+            en: {
+                normal: '2',
+                shifted: '@',
+                double: true
+            },
+            ru: {
+                normal: '2',
+                shifted: '"',
+                double: true
+            }
+        }
+    },
+    {
+        id: 51,
+        size: 1,
+        func: false,
+        name: {
+            en: {
+                normal: '3',
+                shifted: '#',
+                double: true
+            },
+            ru: {
+                normal: '3',
+                shifted: '№',
+                double: true
+            }
+        }
+    },
+    {
+        id: 52,
+        size: 1,
+        func: false,
+        name: {
+            en: {
+                normal: '4',
+                shifted: '$',
+                double: true
+            },
+            ru: {
+                normal: '4',
+                shifted: ';',
+                double: true
+            }
+        }
+    },
+    {
+        id: 53,
+        size: 1,
+        func: false,
+        name: {
+            en: {
+                normal: '5',
+                shifted: '%',
+                double: true
+            },
+            ru: {
+                normal: '5',
+                shifted: '%',
+                double: true
+            }
+        }
+    },
+    {
+        id: 54,
+        size: 1,
+        func: false,
+        name: {
+            en: {
+                normal: '6',
+                shifted: '^',
+                double: true
+            },
+            ru: {
+                normal: '6',
+                shifted: ':',
+                double: true
+            }
+        }
+    },
+    {
+        id: 55,
+        size: 1,
+        func: false,
+        name: {
+            en: {
+                normal: '7',
+                shifted: '&',
+                double: true
+            },
+            ru: {
+                normal: '7',
+                shifted: '?',
+                double: true
+            }
+        }
+    },
+    {
+        id: 56,
+        size: 1,
+        func: false,
+        name: {
+            en: {
+                normal: '8',
+                shifted: '*',
+                double: true
+            },
+            ru: {
+                normal: '8',
+                shifted: '*',
+                double: true
+            }
+        }
+    },
+    {
+        id: 57,
+        size: 1,
+        func: false,
+        name: {
+            en: {
+                normal: '9',
+                shifted: '(',
+                double: true
+            },
+            ru: {
+                normal: '9',
+                shifted: '(',
+                double: true
+            }
+        }
+    },
+    {
+        id: 48,
+        size: 1,
+        func: false,
+        name: {
+            en: {
+                normal: '0',
+                shifted: ')',
+                double: true
+            },
+            ru: {
+                normal: '0',
+                shifted: ')',
+                double: true
+            }
+        }
+    },
+    {
+        id: 189,
+        size: 1,
+        func: false,
+        name: {
+            en: {
+                normal: '-',
+                shifted: '_',
+                double: true
+            },
+            ru: {
+                normal: '-',
+                shifted: '_',
+                double: true
+            }
+        }
+    },
+    {
+        id: 187,
+        size: 1,
+        func: false,
+        name: {
+            en: {
+                normal: '=',
+                shifted: '+',
+                double: true
+            },
+            ru: {
+                normal: '=',
+                shifted: '+',
+                double: true
+            }
+        }
+    },
+    {
+        id: 8,
+        size: 2.5,
+        func: true,
+        name: 'Backspace'
+    },
+    {
+        id: 9,
+        size: 1.5,
+        func: true,
+        name: 'Tab'
+    },
+    {
+        id: 81,
+        size: 1,
+        func: false,
+        name: {
+            en: {
+                normal: 'q',
+                shifted: 'Q',
+                double: false
+            },
+            ru: {
+                normal: 'й',
+                shifted: 'Й',
+                double: false
+            }
+        }
+    },
+    {
+        id: 87,
+        size: 1,
+        func: false,
+        name: {
+            en: {
+                normal: 'w',
+                shifted: 'W',
+                double: false
+            },
+            ru: {
+                normal: 'ц',
+                shifted: 'Ц',
+                double: false
+            }
+        }
+    },
+    {
+        id: 69,
+        size: 1,
+        func: false,
+        name: {
+            en: {
+                normal: 'e',
+                shifted: 'E',
+                double: false
+            },
+            ru: {
+                normal: 'у',
+                shifted: 'У',
+                double: false
+            }
+        }
+    },
+    {
+        id: 82,
+        size: 1,
+        func: false,
+        name: {
+            en: {
+                normal: 'r',
+                shifted: 'R',
+                double: false
+            },
+            ru: {
+                normal: 'к',
+                shifted: 'К',
+                double: false
+            }
+        }
+    },
+    {
+        id: 84,
+        size: 1,
+        func: false,
+        name: {
+            en: {
+                normal: 't',
+                shifted: 'T',
+                double: false
+            },
+            ru: {
+                normal: 'е',
+                shifted: 'Е',
+                double: false
+            }
+        }
+    },
+    {
+        id: 89,
+        size: 1,
+        func: false,
+        name: {
+            en: {
+                normal: 'y',
+                shifted: 'Y',
+                double: false
+            },
+            ru: {
+                normal: 'н',
+                shifted: 'Н',
+                double: false
+            }
+        }
+    },
+    {
+        id: 85,
+        size: 1,
+        func: false,
+        name: {
+            en: {
+                normal: 'u',
+                shifted: 'U',
+                double: false
+            },
+            ru: {
+                normal: 'г',
+                shifted: 'Г',
+                double: false
+            }
+        }
+    },
+    {
+        id: 73,
+        size: 1,
+        func: false,
+        name: {
+            en: {
+                normal: 'i',
+                shifted: 'I',
+                double: false
+            },
+            ru: {
+                normal: 'ш',
+                shifted: 'Ш',
+                double: false
+            }
+        }
+    },
+    {
+        id: 79,
+        size: 1,
+        func: false,
+        name: {
+            en: {
+                normal: 'o',
+                shifted: 'O',
+                double: false
+            },
+            ru: {
+                normal: 'щ',
+                shifted: 'Щ',
+                double: false
+            }
+        }
+    },
+    {
+        id: 80,
+        size: 1,
+        func: false,
+        name: {
+            en: {
+                normal: 'p',
+                shifted: 'P',
+                double: false
+            },
+            ru: {
+                normal: 'з',
+                shifted: 'З',
+                double: false
+            }
+        }
+    },
+    {
+        id: 219,
+        size: 1,
+        func: false,
+        name: {
+            en: {
+                normal: '[',
+                shifted: '{',
+                double: true
+            },
+            ru: {
+                normal: 'х',
+                shifted: 'Х',
+                double: false
+            }
+        }
+    },
+    {
+        id: 221,
+        size: 1,
+        func: false,
+        name: {
+            en: {
+                normal: ']',
+                shifted: '}',
+                double: true
+            },
+            ru: {
+                normal: 'ъ',
+                shifted: 'Ъ',
+                double: false
+            }
+        }
+    },
+    {
+        id: 220,
+        size: 1,
+        func: false,
+        name: {
+            en: {
+                normal: '\\',
+                shifted: '|',
+                double: true
+            },
+            ru: {
+                normal: '\\',
+                shifted: '/',
+                double: true
+            }
+        }
+    },
+    {
+        id: 46,
+        size: 1,
+        func: true,
+        name: 'Del'
+    },
+    {
+        id: 20,
+        size: 2,
+        func: true,
+        name: 'Caps Lock'
+    },
+    {
+        id: 65,
+        size: 1,
+        func: false,
+        name: {
+            en: {
+                normal: 'a',
+                shifted: 'A',
+                double: false
+            },
+            ru: {
+                normal: 'ф',
+                shifted: 'Ф',
+                double: false
+            }
+        }
+    },
+    {
+        id: 83,
+        size: 1,
+        func: false,
+        name: {
+            en: {
+                normal: 's',
+                shifted: 'S',
+                double: false
+            },
+            ru: {
+                normal: 'ы',
+                shifted: 'Ы',
+                double: false
+            }
+        }
+    },
+    {
+        id: 68,
+        size: 1,
+        func: false,
+        name: {
+            en: {
+                normal: 'd',
+                shifted: 'D',
+                double: false
+            },
+            ru: {
+                normal: 'в',
+                shifted: 'В',
+                double: false
+            }
+        }
+    },
+    {
+        id: 70,
+        size: 1,
+        func: false,
+        name: {
+            en: {
+                normal: 'f',
+                shifted: 'F',
+                double: false
+            },
+            ru: {
+                normal: 'а',
+                shifted: 'А',
+                double: false
+            }
+        }
+    },
+    {
+        id: 71,
+        size: 1,
+        func: false,
+        name: {
+            en: {
+                normal: 'g',
+                shifted: 'G',
+                double: false
+            },
+            ru: {
+                normal: 'п',
+                shifted: 'П',
+                double: false
+            }
+        }
+    },
+    {
+        id: 72,
+        size: 1,
+        func: false,
+        name: {
+            en: {
+                normal: 'h',
+                shifted: 'H',
+                double: false
+            },
+            ru: {
+                normal: 'р',
+                shifted: 'Р',
+                double: false
+            }
+        }
+    },
+    {
+        id: 74,
+        size: 1,
+        func: false,
+        name: {
+            en: {
+                normal: 'j',
+                shifted: 'J',
+                double: false
+            },
+            ru: {
+                normal: 'о',
+                shifted: 'О',
+                double: false
+            }
+        }
+    },
+    {
+        id: 75,
+        size: 1,
+        func: false,
+        name: {
+            en: {
+                normal: 'k',
+                shifted: 'K',
+                double: false
+            },
+            ru: {
+                normal: 'л',
+                shifted: 'Л',
+                double: false
+            }
+        }
+    },
+    {
+        id: 76,
+        size: 1,
+        func: false,
+        name: {
+            en: {
+                normal: 'l',
+                shifted: 'L',
+                double: false
+            },
+            ru: {
+                normal: 'д',
+                shifted: 'Д',
+                double: false
+            }
+        }
+    },
+    {
+        id: 186,
+        size: 1,
+        func: false,
+        name: {
+            en: {
+                normal: ';',
+                shifted: ':',
+                double: true
+            },
+            ru: {
+                normal: 'ж',
+                shifted: 'Ж',
+                double: false
+            }
+        }
+    },
+    {
+        id: 222,
+        size: 1,
+        func: false,
+        name: {
+            en: {
+                normal: "'",
+                shifted: '"',
+                double: true
+            },
+            ru: {
+                normal: 'э',
+                shifted: 'Э',
+                double: false
+            }
+        }
+    },
+    {
+        id: 13,
+        size: 2.5,
+        func: true,
+        name: 'Enter'
+    },
+    {
+        id: '16_l',
+        size: 2.5,
+        func: true,
+        name: 'Shift'
+    },
+    {
+        id: 90,
+        size: 1,
+        func: false,
+        name: {
+            en: {
+                normal: 'z',
+                shifted: 'Z',
+                double: false
+            },
+            ru: {
+                normal: 'я',
+                shifted: 'Я',
+                double: false
+            }
+        }
+    },
+    {
+        id: 88,
+        size: 1,
+        func: false,
+        name: {
+            en: {
+                normal: 'x',
+                shifted: 'X',
+                double: false
+            },
+            ru: {
+                normal: 'ч',
+                shifted: 'Ч',
+                double: false
+            }
+        }
+    },
+    {
+        id: 67,
+        size: 1,
+        func: false,
+        name: {
+            en: {
+                normal: 'c',
+                shifted: 'C',
+                double: false
+            },
+            ru: {
+                normal: 'с',
+                shifted: 'С',
+                double: false
+            }
+        }
+    },
+    {
+        id: 86,
+        size: 1,
+        func: false,
+        name: {
+            en: {
+                normal: 'v',
+                shifted: 'V',
+                double: false
+            },
+            ru: {
+                normal: 'м',
+                shifted: 'М',
+                double: false
+            }
+        }
+    },
+    {
+        id: 66,
+        size: 1,
+        func: false,
+        name: {
+            en: {
+                normal: 'b',
+                shifted: 'B',
+                double: false
+            },
+            ru: {
+                normal: 'и',
+                shifted: 'И',
+                double: false
+            }
+        }
+    },
+    {
+        id: 78,
+        size: 1,
+        func: false,
+        name: {
+            en: {
+                normal: 'n',
+                shifted: 'N',
+                double: false
+            },
+            ru: {
+                normal: 'т',
+                shifted: 'Т',
+                double: false
+            }
+        }
+    },
+    {
+        id: 77,
+        size: 1,
+        func: false,
+        name: {
+            en: {
+                normal: 'm',
+                shifted: 'M',
+                double: false
+            },
+            ru: {
+                normal: 'ь',
+                shifted: 'ь',
+                double: false
+            }
+        }
+    },
+    {
+        id: 188,
+        size: 1,
+        func: false,
+        name: {
+            en: {
+                normal: ',',
+                shifted: '<',
+                double: true
+            },
+            ru: {
+                normal: 'б',
+                shifted: 'Б',
+                double: false
+            }
+        }
+    },
+    {
+        id: 190,
+        size: 1,
+        func: false,
+        name: {
+            en: {
+                normal: '.',
+                shifted: '>',
+                double: true
+            },
+            ru: {
+                normal: 'ю',
+                shifted: 'Ю',
+                double: false
+            }
+        }
+    },
+    {
+        id: 191,
+        size: 1,
+        func: false,
+        name: {
+            en: {
+                normal: '/',
+                shifted: '?',
+                double: true
+            },
+            ru: {
+                normal: '.',
+                shifted: ',',
+                double: true
+            }
+        }
+    },        
+    {
+        id: 38,
+        size: 1,
+        func: true,
+        name: '<i class="fas fa-arrow-up"></i>'
+    },
+    {
+        id: '16_r',
+        size: 2,
+        func: true,
+        name: 'Shift'
+    },
+    {
+        id: '17_l',
+        size: 1,
+        func: true,
+        name: 'Ctrl'
+    },
+    {
+        id: 91,
+        size: 1,
+        func: true,
+        name: 'Win'
+    },
+    {
+        id: '18_l',
+        size: 1,
+        func: true,
+        name: 'Alt'
+    },
+    {
+        id: 32,
+        size: 6.5,
+        func: true,
+        name: ''
+    },
+    {
+        id: '18_r',
+        size: 1,
+        func: true,
+        name: 'Alt'
+    },
+    {
+        id: '17_r',
+        size: 1,
+        func: true,
+        name: 'Ctrl'
+    },
+    {
+        id: 37,
+        size: 1,
+        func: true,
+        name: '<i class="fas fa-arrow-left"></i>'
+    },
+    {
+        id: 40,
+        size: 1,
+        func: true,
+        name: '<i class="fas fa-arrow-down"></i>'
+    },
+    {
+        id: 39,
+        size: 1,
+        func: true,
+        name: '<i class="fas fa-arrow-right"></i>'
+    },
+    {
+        id: 'lang',
+        size: 1,
+        func: true,
+        name: {
+                en: 'eng',
+                ru: 'ru'
+            }
+    }
+]
+
+export default keys;
