9e53b09a22e5aa6fb850f7db0d476f577f5b2eca
Toffifi <sanyq@mail.ru>
2020-04-07 18:43:28 +0300

    feat: add modal

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './style.scss';
 import Keyboard from './keyboard';
 import keyboardDriver from './keyboardDriver';
+import modal from './modal/modal';
 
 const main = document.createElement('main');
 const keyboard = document.createElement('section');
@@ -19,4 +20,7 @@ field.innerHTML = '<form> <textarea autofocus class="input"></textarea> </form>'
 const input = document.querySelector('textarea');
 
 const keyboardObj = new Keyboard(input, keyboard, new keyboardDriver);
+
 keyboardObj.initialize();
+
+modal();
---
+++ src/modal/modal.js
@@ -0,0 +1,32 @@
+import './style.scss';
+
+export default function modal() {
+    const modal = document.createElement('div');
+    const overlay = document.createElement('div');
+    const info = document.createElement('div');
+    
+    modal.className = 'modal';
+    overlay.className = 'overlay';
+    info.className = 'info';
+    
+    modal.innerHTML = '<div class="close-button">Close</div><p>Hi! This is a virtual keyboard for Windows. <br> Use "Shift + Ctrl" to change language.</p>';
+    info.innerHTML = '<div class="open-button"><i class="fas fa-info-circle"></i></div>';
+    
+    document.querySelector('body').appendChild(modal);
+    document.querySelector('body').appendChild(overlay);
+    document.querySelector('main').prepend(info);
+    
+    
+    const closeButton = document.querySelector(".close-button");
+    const openButton = document.querySelector(".open-button");
+     
+    closeButton.addEventListener("click", () => {
+       modal.classList.toggle("open");
+       overlay.classList.toggle("open");
+    });
+     
+    openButton.addEventListener("click", () => {
+       modal.classList.toggle("open");
+       overlay.classList.toggle("open");
+    });
+}
